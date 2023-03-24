7730b61d9fbaab987513c3f3f348b4e43b1a1e81
Slutskiy <artikwin3@gmail.com>
2023-03-24 12:34:50 +0300

    feat: added numba in Jupiter

--- CPSO_without_OOP.py
+++ CPSO_without_OOP.py
@@ -19,39 +19,34 @@ def f(x: np.ndarray):  # функция Растригена от -5.12 до 5.1
 
 @njit
 def main(v_max: float):
-    r1 = np.random.rand(2)
-    r2 = np.random.rand(2)
-    weight: float = 3.4
-    p_global_best: float = 1000
-    p_best_coordinates: np.ndarray = np.zeros(2)
+    dimension = 2  # размерность
+    r1 = np.random.rand(dimension - 1)
+    r2 = np.random.rand(dimension - 1)
+    weight = 3.4
+    p_global_best = 1000.0
+    p_best_coordinates = np.zeros(dimension - 1)
 
     for j in range(100_000):
-
-        coordinates = np.array([uniform(-5.12, 5.12), uniform(-5.12, 5.12)])
+        coordinates = np.random.uniform(-5.12, 5.12, dimension - 1)
         c1 = random()
         c2 = random()
-        p_coordinates_now = coordinates
-        p_coordinates = coordinates
+        p_coordinates_now = np.copy(coordinates)
+        p_coordinates = np.copy(coordinates)
         p = f(coordinates)
-        array_of_speed = np.zeros((10, 2))
+        array_of_speed = np.zeros((10, dimension - 1))
 
-        array_of_speed[0][0] = uniform(-1 * v_max, v_max)
-        array_of_speed[0][1] = uniform(-1 * v_max, v_max)
+        for k in range(dimension):
+            array_of_speed[0][k] = uniform(-1 * v_max, v_max)
 
         for i in range(1, len(array_of_speed)):
             array_of_speed[i] = array_of_speed[i - 1] * weight + \
                                 c1 * r1 * (p_best_coordinates - p_coordinates_now) + \
                                 c2 * r2 * (p_best_coordinates - p_coordinates_now)
-
-            if array_of_speed[i][0] > 0:
-                array_of_speed[i][0] = min(array_of_speed[i][0], v_max)
-            else:
-                array_of_speed[i][0] = max(array_of_speed[i][0], v_max)
-
-            if array_of_speed[i][1] > 0:
-                array_of_speed[i][1] = min(array_of_speed[i][1], v_max)
-            else:
-                array_of_speed[i][1] = max(array_of_speed[i][1], v_max)
+            for k in range(dimension):
+                if array_of_speed[i][k] > 0:
+                    array_of_speed[i][k] = min(array_of_speed[i][0], v_max)
+                else:
+                    array_of_speed[i][k] = max(array_of_speed[i][0], v_max)
 
             p_coordinates_now = p_coordinates_now + array_of_speed[i]
 
